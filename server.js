7b317b7653c21101c7bce755020b018e3de4a3dc
Caleb Lay Macbook <caleblaymusic@gmail.com>
2024-04-11 22:10:49 -0600

    Moved html, CSS, and javascript files into "public" directory

--- server.js
+++ server.js
@@ -1,38 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const bodyParser = require('body-parser'); // Required to parse request bodies
 
-// Serve static files from the startup directory
-app.use(express.static('startup'));
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
-
-// Array to store comments (in-memory "database" for demonstration purposes)
-let comments = [
-    { username: 'SuperMario123', comment: 'Wahoo my dudes', timestamp: new Date() },
-    { username: 'Mr. L,', comment: 'you suck mario', timestamp: new Date() },
-    { username: 'Princess Bro-stool', comment: 'Have you guys ever noticed that toad kinda looks like a...', timestamp: new Date() }];
-
-// Endpoints VVV
-
-// Endpoint to get all comments
-app.get('/api/comments', (req, res) => {
-    res.json(comments);
+app.get('/startup/', function(req, res){
+    res.send("Hello from the root application URL");
 });
 
-// Endpoint to add a new comment
-app.post('/api/comments', (req, res) => {
-    const { username, comment } = req.body;
-    if (!username || !comment) {
-        return res.status(400).json({ error: 'Both username and comment are required' });
-    }
-    const newComment = { username, comment, timestamp: new Date() };
-    comments.push(newComment);
-    res.status(201).json(newComment);
+app.get('/mycoolapp/test/', function(req, res){
+    res.send("Hello from the 'test' URL");
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(0, () => console.log('Application is running'));
